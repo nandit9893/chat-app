46e8683cbe7f9403866fff2408485b2877ce4437
Nandit <nanditsharma063@gmail.com>
2024-08-29 09:23:06 +0530

    updated already

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,16 +2,14 @@ import dotenv from "dotenv";
 import connectDB from "./db/database.js";
 import { app } from "./app.js";
 
-dotenv.config({
-    path: "./env"
-})
+dotenv.config();
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () =>{
-        console.log(`Server is running at port : ${process.env.PORT}`)
-    })
-})
-.catch((err) => {
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("Monogo db conncection failed !!!", err);
-})
+  });
